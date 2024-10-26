ff56edb9e455adbe273d62cf10d3cf606ae86f23
Aryan Patil <125805392+aaryan-exe@users.noreply.github.com>
2024-10-26 14:03:10 +0530

    update

--- ping pong.py
+++ ping pong.py
@@ -18,13 +18,21 @@ bg_photo = ImageTk.PhotoImage(bg_image)
 canvas.create_image(0, 0, image=bg_photo, anchor="nw")
 
 ball_image= Image.open("custom_ball.png")
+ball_image = ball_image.resize((ball_size, ball_size), Image.LANCZOS)
 ball_photo=ImageTk.PhotoImage(ball_image)
 ball=canvas.create_image(Width/2, Height/2, image=ball_photo)
 
 left_paddle= canvas.create_rectangle(10, Height/2-PaddleHeight/2, 30, Height/2 + PaddleHeight/2, fill='White')
+right_paddle= canvas.create_rectangle(Width-30, Height/2 - PaddleHeight/2, Width-10, Height/2 + PaddleHeight/2, fill="white")
+
+ball_dx, ball_dy = ball_x, ball_y
+left_paddle_dy=0
+right_paddle_dy=0
+
+def movePaddle():
+    canvas.move(left_paddle, 0, left_paddle_dy)
+    canvas.move(right_paddle, 0, right_paddle_dy)
+    root.after(20, movePaddle)
+
 
 root.mainloop()
-
-
-# left_paddle = canvas.create_rectangle(10, HEIGHT/2 - PADDLE_HEIGHT/2, 30, HEIGHT/2 + PADDLE_HEIGHT/2, fill="white")
-# right_paddle = canvas.create_rectangle(WIDTH - 30, HEIGHT/2 - PADDLE_HEIGHT/2, WIDTH - 10, HEIGHT/2 + PADDLE_HEIGHT/2, fill="white")
